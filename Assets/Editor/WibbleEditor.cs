712d819d1d7a77a23f8c2bfbd744132e54064ff9
svc-student <svc-student@student.directory.jccc>
2019-04-23 17:40:57 -0500

    Test 3 start

---
+++ Assets/Editor/WibbleEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Wibble))]
+public class WibbleEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Wibble wibble = target as Wibble;
+        wibble.wibbleValue = UnboundSlider("Wibblage", -15, 297, wibble.wibbleValue);
+        wibble.wobbleValue = UnboundSlider("Wobblage", 100, 1000, wibble.wobbleValue);
+    }
+
+    private int UnboundSlider(string label, int min, int max, int currentValue)
+    {
+        // TODO: Implement this method as described.
+        throw new System.NotImplementedException("Test 3 not started");
+    }
+
+}
---
+++ Assets/Wibble.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Wibble : MonoBehaviour
+{
+
+    public int wibbleValue = 88;
+    public int wobbleValue = 324;
+
+    void Start()
+    {
+        print(string.Format("I wibble {0} and wobble {1}", wibbleValue, wobbleValue));
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
